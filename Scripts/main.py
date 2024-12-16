ae864378d1b5b0307e3466b46bcf0189dc018fc3
Jens Benoit <jens.benoit@student.ucll.be>
2024-12-16 14:29:02 +0100

    Added a folder for Images and a script for the tileset. I don't think anything changed inside of main? (It looks like it did before but I might just misremember)

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,5 @@
 # Imports of other scripts/logics
 import pygame
-import player
 
 # Initiliasation of pygame
 pygame.init()
@@ -10,12 +9,9 @@ screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
 screen_color = (0, 0, 0)
 Title = pygame.display.set_caption("Sabotage Runners")
 
-
 # The game loop
 running = True
 while running:
-
-    
 
     # Set the screen's color
     screen.fill(screen_color)
