cf0144f4f243cd14e5431aaa9f2b60bfb731737e
Sebastian Brink <sebastian.brink@student.ucll.be>
2024-12-16 16:38:55 +0100

    playermovement

--- Scripts/player.py
+++ Scripts/player.py
@@ -1,10 +1,34 @@
-# Imports of other scripts/logics
 import pygame
 
-# Initiliasation of pygame
-pygame.init()
-
-# Player class
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, size, color, speed):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+        self.speed = speed
+
+    def handle_movement(self, keys, screen_width, screen_height):
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.x += self.speed
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.y -= self.speed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.y += self.speed
+
+        # Keep player within screen bounds
+        self.x = max(0, min(self.x, screen_width - self.size))
+        self.y = max(0, min(self.y, screen_height - self.size))
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.size, self.size))
+
+
+
+
+
+
+
+        
