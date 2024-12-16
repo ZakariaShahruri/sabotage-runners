309baf3a90f72fc7027fad3f24db3db612b090da
Sebastian Brink <sebastian.brink@student.ucll.be>
2024-12-16 12:31:51 +0100

    Experiment with player movement

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,6 @@
 # Imports of other scripts/logics
 import pygame
+import player
 
 # Initiliasation of pygame
 pygame.init()
@@ -9,9 +10,12 @@ screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
 screen_color = (0, 0, 0)
 Title = pygame.display.set_caption("Sabotage Runners")
 
+
 # The game loop
 running = True
 while running:
+
+    
 
     # Set the screen's color
     screen.fill(screen_color)
@@ -19,6 +23,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
     
     # Update the display
     pygame.display.flip()
