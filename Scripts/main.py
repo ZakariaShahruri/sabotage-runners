c18a945b2756a41e0b63f3d395439d28dd7bfb95
Moad Khaouili <moad.khaouili@student.ucll.be>
2024-12-16 20:31:25 +0100

    Add player sprite and background image

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,13 +6,13 @@ from player import Player
 pygame.init()
 
 # Display and Title settings
-screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+screen = pygame.display.set_mode((1950, 1300), pygame.RESIZABLE)
 screen_color = (0, 0, 0)
 Title = pygame.display.set_caption("Sabotage Runners")
+clock = pygame.time.Clock()
+background_surface = pygame.image.load('images/dcbc8b76-6720-4fe2-91fd-0b418cedfa3e.webp')
 
-# Defined player in main [Adds more readability this way imo, also sped up the player as it felt quite slow :)]
-player = Player(x=300, y=200, size=40, color=(0, 255, 0), speed=3)
-
+player = Player(x=300, y=200, size=40, speed=7)
 # The game loop
 running = True
 while running:
@@ -23,8 +23,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    # Player handling [potentially better way to section this off? just a suggestion]
+    screen.blit(background_surface,(0,0))
     keys = pygame.key.get_pressed()
     # Update player position
     player.handle_movement(keys, screen.get_width(), screen.get_height())
@@ -33,16 +32,9 @@ while running:
     
     
     # Update the display
-    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(120)
 
-    # Limit FPS to 60 [This should fix the weird feeling of the player movement]
-    pygame.time.Clock().tick(60)
-
-# This is essentially the opposite of pygame.init() and closes the pygame library, might have to be moved to the bottom of the script.
-# [Quick note, I am not sure if this is even necessary, I added it just in case]
-pygame.quit()
-
-# Not sure if we should switch this around with the game loop or not. It feels weird to me to define classes and stuff after having started the game loop.
 class State:
     def __init__(self):
         self.x = 0
