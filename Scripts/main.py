56d2bb6bf3c68c8f13f5b71cbff8d26a27ecd30e
Moad Khaouili <moad.khaouili@student.ucll.be>
2024-12-16 20:31:25 +0100

    Adding the player model and a background

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,11 +6,13 @@ from player import Player
 pygame.init()
 
 # Display and Title settings
-screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+screen = pygame.display.set_mode((1950, 1300), pygame.RESIZABLE)
 screen_color = (0, 0, 0)
 Title = pygame.display.set_caption("Sabotage Runners")
+clock = pygame.time.Clock()
+background_surface = pygame.image.load('images/dcbc8b76-6720-4fe2-91fd-0b418cedfa3e.webp')
 
-player = Player(x=300, y=200, size=40, color=(0, 255, 0), speed=.5)
+player = Player(x=300, y=200, size=40, speed=7)
 # The game loop
 running = True
 while running:
@@ -21,7 +23,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+    screen.blit(background_surface,(0,0))
     keys = pygame.key.get_pressed()
     # Update player position
     player.handle_movement(keys, screen.get_width(), screen.get_height())
@@ -30,7 +32,8 @@ while running:
     
     
     # Update the display
-    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(120)
 
 class State:
     def __init__(self):
