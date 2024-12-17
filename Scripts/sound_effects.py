8181372d907df5e368274ef08e52f816390d2374
Zakaria Shahruri <zakaria.shahruri@student.ucll.be>
2024-12-17 10:39:16 +0100

    Split audio.py into music_manager.py and sound_effects.py

---
+++ Scripts/sound_effects.py
@@ -0,0 +1,86 @@
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+# Path to sound effects
+banana_slip_fx = "../audios/banana_slip_fx.mp3"
+freezed_fx = "../audios/freezed_fx.mp3"
+speed_up_fx = "../audios/speed_up_fx.mp3"
+slow_down_fx = "../audios/slow_down_fx.mp3"
+teleport_fx = "../audios/teleport_fx.mp3"
+shield_fx = "../audios/shield_fx.mp3"
+mirrored_fx = "../audios/mirrored_fx.mp3"
+
+# Sound Effect Manager Class
+class SoundEffects:
+    def __init__(self):
+        """Load sound effects into memory."""
+        self.banana_slip = pygame.mixer.Sound(banana_slip_fx)
+        self.freezed = pygame.mixer.Sound(freezed_fx)
+        self.speed_up = pygame.mixer.Sound(speed_up_fx)
+        self.slow_down = pygame.mixer.Sound(slow_down_fx)
+        self.teleport = pygame.mixer.Sound(teleport_fx)
+        self.shield = pygame.mixer.Sound(shield_fx)
+        self.mirrored = pygame.mixer.Sound(mirrored_fx)
+    
+    def play_banana_slip(self):
+        """Play the banana slip sound effect."""
+        self.banana_slip.play()
+    
+    def play_freeze(self):
+        """Play the freeze power-up sound effect."""
+        self.freezed.play()
+    
+    def play_speed_up(self):
+        """Play the speed-up power-up sound effect."""
+        self.speed_up.play()
+    
+    def play_slow_down(self):
+        """Play the slow-down power-up sound effect."""
+        self.slow_down.play()
+    
+    def play_teleport(self):
+        """Play the teleport sound effect."""
+        self.teleport.play()
+
+    def play_shield(self):
+        """Play the shield sound effect."""
+        self.shield.play()
+    
+    def play_mirrored(self):
+        """Play the mirrored power-up sound effect."""
+        self.mirrored.play()
+
+# Example Usage
+if __name__ == "__main__":
+    try:
+        # Initialize sound effects
+        sound_effects = SoundEffects()
+        print("Sound effects test started...")
+
+        input("Press Enter to play banana slip sound...")
+        sound_effects.play_banana_slip()
+
+        input("Press Enter to play freeze sound...")
+        sound_effects.play_freeze()
+
+        input("Press Enter to play speed-up sound...")
+        sound_effects.play_speed_up()
+
+        input("Press Enter to play slow down sound...")
+        sound_effects.play_slow_down()
+
+        input("Press Enter to play teleport sound...")
+        sound_effects.play_teleport()
+
+        input("Press Enter to play shield sound...")
+        sound_effects.play_shield()
+
+        input("Press Enter to play mirrored sound...")
+        sound_effects.play_mirrored()
+
+        print("Exiting sound effects test...")
+
+    finally:
+        pygame.quit()
