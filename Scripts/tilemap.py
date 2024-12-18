db9153b3146aabc20c96a02d06765bf42f8fb982
Jens Benoit <jens.benoit@student.ucll.be>
2024-12-18 12:28:38 +0100

    made it so we can easily switch between images

--- Scripts/tilemap.py
+++ Scripts/tilemap.py
@@ -57,16 +57,17 @@ tilemap_2 = [
     '........................................',
     '........................................',
     '........................................',
+    '........................................',
     'WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW',
 ]
 
-def draw_map(tilemap, screen):
+def draw_map(tilemap, image, screen):
     walls = []
 
     for y, row in enumerate(tilemap):
         for x, tile in enumerate(row):
             if tile == 'W':
-                wall = State(x * tilesize, y * tilesize, '../Images/fence.png', size=tilesize, is_collidable=True)
+                wall = State(x * tilesize, y * tilesize, image, size=tilesize, is_collidable=True)
                 walls.append(wall)
 
                 wall.render(screen)
