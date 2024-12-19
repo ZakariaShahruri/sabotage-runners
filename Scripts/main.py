76cc0da4d7c2f0283ce98e8488e9e94299de6662
Jens Benoit <jens.benoit@student.ucll.be>
2024-12-19 20:45:27 +0100

    Small changes to menu_screen.py (title and made it resizable) and main.py (removed unused code)

--- Scripts/main.py
+++ Scripts/main.py
@@ -126,12 +126,6 @@ def game_loop():
                     game_logic.player2.x -= 30
                 elif rectangle.colliderect(game_logic.player1.get_rect()):
                     game_logic.player1.x += 30
-        
-        # for rectangle in all_rectangles2:
-        #         if rectangle.colliderect(game_logic.player2.get_rect()):
-        #             game_logic.player2.x -= 30
-        #         elif rectangle.colliderect(game_logic.player1.get_rect()):
-        #             game_logic.player1.x += 30
 
         if game_logic.player1.getting_hit:
             current_p2_cooldown = pygame.time.get_ticks()
