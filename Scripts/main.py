08f39e01f7cc429ee6389e7b0a6fe62773551f56
Jens Benoit <jens.benoit@student.ucll.be>
2024-12-19 18:58:14 +0100

    Collision tiles added along with the logic to keep players from getting stuck in walls

--- Scripts/main.py
+++ Scripts/main.py
@@ -119,7 +119,13 @@ def game_loop():
             game_logic.player1.getting_hit = True
             cooldown_start = pygame.time.get_ticks()
 
-        for rectangle in all_rectangles:
+        for rectangle in all_rectangles1:
+                if rectangle.colliderect(game_logic.player2.get_rect()):
+                    game_logic.player2.x -= 30
+                elif rectangle.colliderect(game_logic.player1.get_rect()):
+                    game_logic.player1.x += 30
+        
+        for rectangle in all_rectangles2:
                 if rectangle.colliderect(game_logic.player2.get_rect()):
                     game_logic.player2.x -= 30
                 elif rectangle.colliderect(game_logic.player1.get_rect()):
@@ -145,6 +151,7 @@ def game_loop():
         game_logic.player2.render(screen)
             
         screen.blit(hedges, (0,0))
+
         # Render walls
         borders = render_border(map1_borders, screen)
 
