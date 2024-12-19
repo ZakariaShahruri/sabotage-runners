9d3b970193e1d5415b486644166ac665116ce354
Ayden Schwindt <ayden.schwindt@student.ucll.be>
2024-12-19 09:15:22 +0100

    attack animation/ boolean created

--- Scripts/game_logic.py
+++ Scripts/game_logic.py
@@ -41,6 +41,7 @@ class GameLogic:
         self.run1 = ["../Images/player1/player1_run1.png", "../Images/player1/player1_run2.png", "../Images/player1/player1_run3.png", "../Images/player1/player1_run4.png", "../Images/player1/player1_run5.png", "../Images/player1/player1_run6.png", "../Images/player1/player1_run7.png", "../Images/player1/player1_run8.png"]
         self.run2 = ["../Images/player2/player2_run1.png", "../Images/player2/player2_run2.png", "../Images/player2/player2_run3.png", "../Images/player2/player2_run4.png", "../Images/player2/player2_run5.png", "../Images/player2/player2_run6.png", "../Images/player2/player2_run7.png", "../Images/player2/player2_run8.png"]
         self.attack1 = ["../Images/player1/player1_attack1.png", "../Images/player1/player1_attack2.png", "../Images/player1/player1_attack3.png", "../Images/player1/player1_attack4.png", "../Images/player1/player1_attack5.png", "../Images/player1/player1_attack6.png", "../Images/player1/player1_attack7.png", "../Images/player1/player1_attack8.png"]
+        self.attack2 = ["../Images/player1/player1_attack1.png", "../Images/player1/player1_attack2.png", "../Images/player1/player1_attack3.png", "../Images/player1/player1_attack4.png", "../Images/player1/player1_attack5.png", "../Images/player1/player1_attack6.png", "../Images/player1/player1_attack7.png", "../Images/player1/player1_attack8.png"]
         
         # Item management
         self.active_items = []
@@ -129,16 +130,24 @@ class GameLogic:
     
     def animate_players(self):
         """Animate players with idle animations"""
-        if self.player1.is_moving == False:
-            self.player1.animate(self.idle1, 0.1)
-        else:
+        if self.player1.is_moving == True and self.player1.attack == True:
+            self.player1.animate(self.attack1, 0.5)
+        elif self.player1.is_moving == True:
             self.player1.animate(self.run1, .2)
-
-
-        if self.player2.is_moving == False:
-            self.player2.animate(self.idle2, 0.1)
+        elif self.player1.is_moving == False and self.player1.attack == True:
+            self.player1.animate(self.attack1, 0.5)
         else:
+            self.player1.animate(self.idle1, .1)
+
+        if self.player2.is_moving == True and self.player2.attack == True:
+            self.player2.animate(self.attack2, 0.5)
+        elif self.player2.is_moving == True:
             self.player2.animate(self.run2, .2)
+        elif self.player2.is_moving == False and self.player2.attack == True:
+            self.player2.animate(self.attack2, 0.5)
+        else:
+            self.player2.animate(self.idle2, .1)
+
 
 
     def render_scores(self, screen):
