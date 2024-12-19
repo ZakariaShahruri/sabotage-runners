9b8d038f7124109aaa991df4546d9d21f7b100c8
Moad Khaouili <moad.khaouili@student.ucll.be>
2024-12-19 16:07:08 +0100

    removing the hitboxes

--- Scripts/state.py
+++ Scripts/state.py
@@ -43,8 +43,8 @@ class State:
 
         screen.blit(self.image, (self.x,self.y))
 
-        rect_color = (255,0,0)
-        pygame.draw.rect(screen, rect_color, self.get_rect(), 2)
+        # rect_color = (255,0,0)
+        # pygame.draw.rect(screen, rect_color, self.get_rect(), 2)
 
     def get_rect(self):
 
